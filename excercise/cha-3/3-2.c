33c5223431977b7c88d5b14a5e31020f01c74651
ikow <qideng@usc.edu>
2016-02-25 23:49:08 -0800

    Molar Mass

---
+++ excercise/cha-3/3-2.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<string.h>
+
+const float C1 = 12.01;
+const float H1 = 1.008;
+const float O1 = 16.00;
+const float N1 = 14.01;
+
+int main()
+{
+    char s[50];
+    float S = 0;
+    scanf("%s", s);
+    for(int i = 0; i < strlen(s); i++)
+    {
+        if(!isalpha(s[i+1]))
+    }
+}
